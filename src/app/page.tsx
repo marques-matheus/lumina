29fbd5306530a59a7130f5bf5139668ebb7a5e40
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2025-06-14 11:30:02 -0300

    fix: sorting by activity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function ProductsPage() {
   const { data: products } = await supabase
     .from('products')
     .select('*')
-    .order('created_at, is_active', { ascending: false })
+    .order('is_active', { ascending: false })
 
   return (
     <div className="flex flex-col gap-8">
