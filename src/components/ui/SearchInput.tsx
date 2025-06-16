222cc358454f7ff1acf2281a61a0a0fe3216a667
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2025-06-16 15:17:09 -0300

    feat: searchInput

---
+++ src/components/ui/SearchInput.tsx
@@ -0,0 +1,42 @@
+'use client';
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { Input } from "./input";
+import { Button } from "./button";
+import { Search } from "lucide-react";
+
+export default function SearchInput({ placeholder }: { placeholder: string }) {
+
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    function handleSearch(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        const formData = new FormData(event.currentTarget);
+        const term = formData.get("search") as string;
+
+        const params = new URLSearchParams(searchParams.toString());
+
+        if (term) {
+            params.set("search", term);
+        } else {
+            params.delete("search");
+        }
+
+        replace(`${pathname}?${params.toString()}`);
+    }
+
+    return (
+        <form onSubmit={handleSearch} className="flex items-center" >
+            <Input
+                className="rounded-r-none"
+                name="search"
+                placeholder={placeholder}
+                defaultValue={searchParams.get('search')?.toString() || ''}
+            />
+            <Button className="rounded-l-none" type="submit"><Search className="h-4 w-4" /></Button>
+        </form>
+    )
+
+}
